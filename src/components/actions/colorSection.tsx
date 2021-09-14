5a3f71522b5b70e5ffa4b201a42c7ac87f3457a2
Kyle Ki <kki@pop-os.localdomain>
2021-09-14 13:55:55 -0400

    Split the newEventModal into several files.

---
+++ src/components/actions/colorSection.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from './actions.module.scss';
+
+type ColorSectionProps = {
+  selected: string,
+  setValue: ((value: string) => void),
+};
+
+const ColorSection = (props: ColorSectionProps): JSX.Element => {
+  const { selected, setValue } = props;
+
+  return (
+    <div className={styles['color-selection']}>
+      <button
+        type="button"
+        className={[selected === 'green' ? styles.selected : null, styles.green, styles['color-button']].join(' ')}
+        aria-label="Color Green"
+        onClick={() => setValue('green')}
+      />
+      <button
+        type="button"
+        className={[selected === 'lightgreen' ? styles.selected : null, styles.lightgreen, styles['color-button']].join(' ')}
+        aria-label="Color Lightgreen"
+        onClick={() => setValue('lightgreen')}
+      />
+      <button
+        type="button"
+        id="blue"
+        className={[selected === 'blue' ? styles.selected : null, styles.blue, styles['color-button']].join(' ')}
+        aria-label="Color Blue"
+        onClick={() => setValue('blue')}
+      />
+      <button
+        type="button"
+        id="red"
+        className={[selected === 'red' ? styles.selected : null, styles.red, styles['color-button']].join(' ')}
+        aria-label="Color Red"
+        onClick={() => setValue('red')}
+      />
+      <button
+        type="button"
+        id="orange"
+        className={[selected === 'orange' ? styles.selected : null, styles.orange, styles['color-button']].join(' ')}
+        aria-label="Color Orange"
+        onClick={() => setValue('orange')}
+      />
+    </div>
+  );
+};
+
+export default ColorSection;
